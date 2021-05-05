113c65e581194ecbf3b1bfb76ca9c60285f51c1d
imarco20 <marcode420@gmail.com>
2021-05-05 21:55:31 +0200

    Handle different scenarios of entered script arguments and invalid file formats

--- parser.py
+++ parser.py
@@ -1,15 +1,31 @@
+import json
 import sys
+import os
 
 from json_converter_factory import JsonConverterFactory
 from parser_factory import ParserFactory
 
 
 def main(argv):
-    # TODO: Try Except
-    format = argv[0]
-    files = argv[1:]
+    try:
+        format = argv[0]
+    except IndexError:
+        print("You didn't specify a data format. Please choose either CSV or XML")
+        sys.exit()
 
-    parser = ParserFactory.create_parser(format)
+    try:
+        files = argv[1:]
+    except IndexError:
+        print(
+            "Not enough arguments. Please specify a valid format, and file(s) that you want to parse and convert to JSON.")
+        sys.exit()
+
+    if format in ["csv", "xml"]:
+        parser = ParserFactory.create_parser(format)
+    else:
+        print(
+            "The specified format is either an invalid format or one that we don't support. We currently support CSV and XML formats only.")
+        sys.exit()
 
     data = []
 
@@ -17,9 +33,18 @@ def main(argv):
         data.append(parser.parse(file))
 
     converter = JsonConverterFactory.create_json_converter(format)
-    return converter.convert(data)
+
+    current_directory = os.path.dirname(os.path.realpath(__file__))
+    parsing_directory = current_directory + "/parsing_result"
+
+    if not os.path.exists(parsing_directory):
+        os.mkdir(parsing_directory)
+
+    json_file_path = os.path.join(parsing_directory, 'sample.json')
+
+    with open(json_file_path, 'w') as f:
+        json.dump(converter.convert(data), f)
 
 
 if __name__ == "__main__":
-    result = main(sys.argv[1:])
-    print(result)
+    main(sys.argv[1:])
